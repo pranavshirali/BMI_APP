1c70d5ee72cde4c49044a0f4d7a54963f96b0149
pranavshirali <pranavshirali007@gmail.com>
2023-05-30 22:11:43 +0530

    Modified this codes

--- app/src/main/java/com/appleassociates/bmi/MainActivity.java
+++ app/src/main/java/com/appleassociates/bmi/MainActivity.java
@@ -19,35 +19,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnCal;
-        EditText weight, heightft, heightin;
+        EditText weight, heightft;
         TextView result;
 
         btnCal = findViewById(R.id.buttoncal);
         weight = findViewById(R.id.editWeight);
         heightft = findViewById(R.id.editHeightFt);
-        heightin = findViewById(R.id.editHeightInc);
         result = findViewById(R.id.txtResult);
 
         btnCal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int wt = Integer.parseInt(weight.getText().toString());
-                int feet = Integer.parseInt(heightft.getText().toString());
-                int inch = Integer.parseInt(heightin.getText().toString());
 
-                int totalIn = feet*12 + inch;
-                double totalCm = totalIn * 2.53;
-                double totalM = totalCm / 100;
+                int wt = (int) Double.parseDouble(weight.getText().toString());
+                int feet = (int) Double.parseDouble(heightft.getText().toString());
+                double totalM = feet * 0.305;
 
                 double bmi = wt / (totalM*totalM);
 
                 if (bmi > 25){
-                    result.setText("You are overweight!!");
-                } else if (bmi < 18) {
-                    result.setText("Your are underweight");
+                    result.setText(R.string.overweight);
+                } else if (bmi < 18.5) {
+                    result.setText(R.string.underweight);
                 }
                 else {
-                    result.setText("You are healty");
+                    result.setText(R.string.healthy);
                 }
             }
         });
